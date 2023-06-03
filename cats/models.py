65f16978a01b688ebbbd207282d6ff4720d8a931
yevdem82 <evdem82@yandex.ru>
2023-06-03 22:18:13 +0300

    Add achievements to cat

--- cats/models.py
+++ cats/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 
+class Achievement(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
 class Owner(models.Model):
     first_name = models.CharField(max_length=128)
     last_name = models.CharField(max_length=128)
@@ -18,6 +25,18 @@ class Cat(models.Model):
         related_name='cats',
         on_delete=models.CASCADE,
     )
+    achievements = models.ManyToManyField(
+        Achievement,
+        through='AchievementCat'
+    )
 
     def __str__(self):
         return self.name
+
+
+class AchievementCat(models.Model):
+    achievement = models.ForeignKey(Achievement, on_delete=models.CASCADE)
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.achievement} {self.cat}'
--- cats/serializers.py
+++ cats/serializers.py
@@ -1,15 +1,40 @@
 from rest_framework import serializers
 
-from .models import Cat, Owner
+from .models import Cat, Owner, Achievement, AchievementCat
+
+
+class AchievementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Achievement
+        fields = ('id', 'name')
 
 
 class CatSerializer(serializers.ModelSerializer):
+    achievements = AchievementSerializer(many=True)
+
+    # owner = serializers.StringRelatedField(read_only=True)
+
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
+
+    def create(self, validated_data):
+        achievements = validated_data.pop('achievements')
+        cat = Cat.objects.create(**validated_data)
+
+        for achievement in achievements:
+            current_achievement, status = Achievement.objects.get_or_create(
+                **achievement)
+            AchievementCat.objects.create(
+                achievement=current_achievement,
+                cat=cat,
+            )
+        return cat
 
 
 class OwnerSerializer(serializers.ModelSerializer):
+    cats = serializers.StringRelatedField(many=True, read_only=True)
+
     class Meta:
         model = Owner
-        fields = ('first_name', 'last_name')
+        fields = ('first_name', 'last_name', 'cats')
