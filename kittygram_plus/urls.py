993a6b6f00949f070bd82cfdcb064273c6a03c63
yevdem82 <evdem82@yandex.ru>
2023-06-04 16:36:05 +0300

    Add some functions

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -10,4 +10,6 @@ router.register('owners', OwnerViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
